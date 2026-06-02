2f061a496490e116cfee302360a1f48578276fcf
Rostislav Dugin <rostislav.dugin@outlook.com>
2026-06-02 17:56:53 +0000

    merge migrations

--- backend/migrations/20260531010000_add_physical_force_full_request.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE physical_backup_configs
-    ADD COLUMN force_full_requested_at TIMESTAMPTZ;
-
--- +goose Down
-ALTER TABLE physical_backup_configs
-    DROP COLUMN force_full_requested_at;
